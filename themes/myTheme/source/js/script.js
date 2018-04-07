ccb5e19a9cd9a769cb2d55f86dc37f62327843d4
bowenzhai <kevinzhai97@gmail.com>
2018-04-07 00:47:42 -0400

    sidebar transition

--- themes/myTheme/source/js/script.js
+++ themes/myTheme/source/js/script.js
@@ -12,4 +12,24 @@ $(function() {
 			}
 		}
 	});
+
+	// on scroll
+	$(window).scroll(function() {
+		var windowPos = $(window).scrollTop();
+		$("body > div").slice(1).each(function (index) {
+			if (windowPos < $(this).position().top) {
+				setActive(index);
+				return false;
+			}
+		});
+	});
+
+	// set active nav link
+	function setActive(index) {
+		var translateAmout = index * -56;
+		$("#side-nav-links .current-section").removeClass("current-section");
+		$("#side-nav-links a").eq(index).addClass("current-section");
+		var translation = "translateY(" + translateAmout + "px)";
+		$("#side-nav-links").css("transform", translation);
+	}
 });
